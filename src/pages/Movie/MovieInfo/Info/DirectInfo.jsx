8b2a94d66d8e9b578642a6055880e05b2765ac8a
tuilaqui123 <quidhtv0149@gmail.com>
2024-03-17 16:20:09 +0700

    actor

--- src/pages/Movie/MovieInfo/Info/DirectInfo.jsx
+++ src/pages/Movie/MovieInfo/Info/DirectInfo.jsx
@@ -1,62 +1,18 @@
 import './info.css'
 import React from 'react';
 import actor from '../../../../assets/images/actor.jpg'
+import movie from '../../../../assets/movies/1.jpg'
+import InfoBox from './InfoBox';
 
 const DirectInfo = () => {
     return (
         <div className='info-container'>
-            <ul className='direct-container'>
-                <li>
-                    <img
-                        src={actor}
-                        alt='actor'
-                    />
-                    <p className='direct-name'>Laurent Laurent  </p>
-                    <p className='character-name'>Laurent Laurent </p>
-                </li>
-
-                <li>
-                    <img
-                        src={actor}
-                        alt='actor'
-                    />
-                    <p className='direct-name'>Laurent Laurent </p>
-                    <p className='character-name'>Laurent (voice)</p>
-                </li>
-                <li>
-                    <img
-                        src={actor}
-                        alt='actor'
-                    />
-                    <p className='direct-name'>Laurent Laurent </p>
-                    <p className='character-name'>Laurent (voice)</p>
-                </li>
-                <li>
-                    <img
-                        src={actor}
-                        alt='actor'
-                    />
-                    <p className='direct-name'>Laurent Laurent </p>
-                    <p className='character-name'>Laurent (voice)</p>
-                </li>
-                <li>
-                    <img
-                        src={actor}
-                        alt='actor'
-                    />
-                    <p className='direct-name'>Laurent Laurent </p>
-                    <p className='character-name'>Laurent (voice)</p>
-                </li>
-                <li>
-                    <img
-                        src={actor}
-                        alt='actor'
-                    />
-                    <p className='direct-name'>Laurent Laurent </p>
-                    <p className='character-name'>Laurent (voice)</p>
-                </li>
-
-            </ul>
+            <div className='direct-container'>
+                <InfoBox />
+                <InfoBox />
+                <InfoBox />
+                <InfoBox />
+            </div>
         </div>
     );
 }
