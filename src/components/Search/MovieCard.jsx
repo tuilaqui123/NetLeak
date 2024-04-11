165fe00280ef8ad2ea50828e2788143eda6201e0
TeddyToT <146633707+TeddyToT@users.noreply.github.com>
2024-04-11 15:12:14 +0700

    Update MovieCard.jsx

--- src/components/Search/MovieCard.jsx
+++ src/components/Search/MovieCard.jsx
@@ -3,11 +3,11 @@ import React  from 'react';
 const MovieCard = ({title, img}) => {
     return (
         
-            <section className='m-4 p-4 cursor-pointer'>
-                <img className='w-80 mb-4' src={img}
+            <section className='w-64 m-4 p-4 cursor-pointer hover:scale-125'>
+                <img className='w-full h-full mb-4 rounded-lg' src={img}
                 alt=""/>
                 <div className='mb-4 text-center text-white'>
-                    <p>{title}</p>
+                    <p className='truncate'>{title}</p>
                 </div>
             </section>
             
