f4f4f4873497a91d2fd004e62b0146f72a29f3db
TeddyToT <146633707+TeddyToT@users.noreply.github.com>
2024-04-11 15:12:39 +0700

    Update SearchInput.jsx

--- src/components/Search/SearchInput.jsx
+++ src/components/Search/SearchInput.jsx
@@ -6,7 +6,7 @@ return (
     {/* <h2> Tìm kiếm</h2> */}
     <form onSubmit={e => { e.preventDefault(); }} className='w-[60%]'>
         <div className='mt-3 flex justify-between shadow-md border-2 boder-grey-200 rounded-lg w-[100%] h-[170%] text-white'>
-        <input className='w-full outline-none rounded-md bg-neutral-700'
+        <input className='pl-3 w-full outline-none rounded-md bg-neutral-700 italic'
         type="text" 
         placeholder='Tìm tên phim, diễn viên,...'
         value={query}
