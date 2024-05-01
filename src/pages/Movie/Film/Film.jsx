83482fd1b6212fb46c76cbbdf8cbb8dff806c685
ThuHuong03 <ntthuong010603@gmail.com>
2024-05-02 02:43:29 +0700

    update film.jsx

--- src/pages/Movie/Film/Film.jsx
+++ src/pages/Movie/Film/Film.jsx
@@ -2,18 +2,154 @@ import React, { useState, useEffect } from 'react';
 import thumb_pic from '../../../assets/images/thumbnail.jpg'
 import './film.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPlay, faPlus, faStar } from '@fortawesome/free-solid-svg-icons';
+import { faPlay, faPlus,faHeart, faStar } from '@fortawesome/free-solid-svg-icons';
 import { useNavigate, useParams } from 'react-router-dom';
 import axios from "axios";
 import Rating from './rating';
 import clsx from 'clsx';
+import IWantEvaluate from '../../Video/IWantEvaluate';
+import { jwtDecode } from 'jwt-decode';
 
 const Film = ({ film }) => {
 
     const navigate = useNavigate()
+    const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY2MjM3MTJiZGJjODRjNzZmYjkwMWI4OSIsImVtYWlsIjoidHJhbm5odXRwaGF0dHZAZ21haWwuY29tIiwiaWF0IjoxNzE0NTg0MjA4LCJleHAiOjE3MTQ2MDIyMDh9.PPCLA9mHxdS_wPyplsIHWh3lGmm9MLwm-MEB8fevS38'
+const userId = jwtDecode(token).id
 
+    const [allRatingFilms, setAllRatingFilms] = useState([])
+    const [allFavoriteFilms, setAllFavoriteFilms] = useState([])
     const [isRating, setIsRating] = useState(false)
+    const handleAddRating = (filmId, rate) => {
+        fetch(`http://localhost:8081/v1/api/user/rating`, {
+            method: 'POST',
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": `Bearer ${token}`
+            },
+            body: JSON.stringify({
+                emailId: userId,
+                filmId: filmId,
+                rate: rate
+            })
+        })
+            .then(() => {
+                getAllRatingFilm()
+            })
+            .catch((e) => {
+                console.log(e)
+            })
+    }
+    const handleDeleteRating = (film) => {
+        fetch(`http://localhost:8081/v1/api/user/rating`, {
+            method: 'DELETE',
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": `Bearer ${token}`
+            },
+            body: JSON.stringify({
+                userId: userId,
+                filmId: film._id
+            })
+        })
+            .then(() => {
+                getAllRatingFilm()
+            })
+            .catch((e) => {
+                console.log(e)
+            })
+    }
+    const getAllFavoriteFilms = () => {
+        // setIsLoadingAllFavoriteFilm(true)
+        fetch(`http://localhost:8081/v1/api/user/favorite/${userId}`, {
+            method: 'GET',
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": `Bearer ${token}`
+            },
+        })
+            .then((res) => res.json())
+            .then((data) => {
+                if (data.success != false)
+                    setAllFavoriteFilms(data)
+            })
+            .catch((e) => {
+                console.log(e)
+            })
+            .finally(() => {
+                // setIsLoadingAllFavoriteFilm(false)
+            })
+    }
+   
 
+    const getAllRatingFilm = () => {
+        // setIsLoadingAllRatingFilm(true)
+        fetch(`http://localhost:8081/v1/api/user/ratings`, {
+            method: 'GET',
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": `Bearer ${token}`
+            },
+        })
+            .then((res) => res.json())
+            .then((data) => {
+                setAllRatingFilms(data)
+            })
+            .catch((e) => {
+                console.log(e)
+            })
+            .finally(() => {
+                // setIsLoadingAllRatingFilm(false)
+            })
+    }
+    const handleAddFavoriteFilm = (filmId) => {
+        fetch(`http://localhost:8081/v1/api/user/favorite`, {
+            method: 'POST',
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": `Bearer ${token}`
+            },
+            body: JSON.stringify({
+                filmId: filmId,
+                userId: userId
+            })
+        })
+            .then(() => {
+                getAllFavoriteFilms()
+            })
+            .catch((e) => {
+                console.log(e)
+            })
+    }
+
+    const handleDeleteFavoriteFilm = (filmId) => {
+        fetch(`http://localhost:8081/v1/api/user/favorite`, {
+            method: 'DELETE',
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": `Bearer ${token}`
+            },
+            body: JSON.stringify({
+                filmId: filmId,
+                userId: userId
+            })
+        })
+            .then(() => {
+                getAllFavoriteFilms()
+            })
+            .catch((e) => {
+                console.log(e)
+            })
+    }
+    useEffect(() => {
+        // getFilmInfo()
+        // getFilmVideo()
+        // getAllGenres()
+        // getAllFilms()
+        // getRecommendFilms()
+        getAllFavoriteFilms()
+        // getRankingFilms()
+        getAllRatingFilm()
+    }, [])
 
     return (
         <div className='w-full h-3/4 relative flex flex-col-reverse justify-between lg:flex-row mt-20 '>
@@ -27,23 +163,10 @@ const Film = ({ film }) => {
                         <FontAwesomeIcon icon={faStar} className='ml-1 text-green-500' />
                     </li>
                     <div className='w-[150px] relative ml-3'>
-                        <button
-                            onClick={() => setIsRating(!isRating)}
-                            className='h-full w-auto border px-2 py-1 rounded-md hover:bg-white duration-150 group'
-                        // className={clsx({
-                        //     'h-full w-auto border px-2 py-1 rounded-md hover:bg-white duration-150 group': isRating,
-                        //     'h-full w-auto border px-2 py-1 rounded-md hover:bg-white duration-150 group': !isRating
-                        // })}
-                        >
-                            {isRating ? (
-                                <p className='text-red-600 font-bold duration-150'>Xác nhận</p>
-                            ) : (
-                                <p className='group-hover:text-[#3e3e3e] font-bold duration-150'>Đánh giá</p>
-                            )}
-                        </button>
-                        {isRating && (
-                            <Rating />
-                        )}
+                       <IWantEvaluate  handleAddRating={handleAddRating}
+                    handleDeleteRating={handleDeleteRating}
+                    allRatingFilms={allRatingFilms}
+                    filmId={film._id}/>
                     </div>
                 </ul>
                 <div className='flex flex-row flex-wrap gap-3 mb-5 movie-tag'>
@@ -61,10 +184,25 @@ const Film = ({ film }) => {
                         <FontAwesomeIcon icon={faPlay} className='icon' />
                         <p>Phát</p>
                     </button>
-                    <button className='playlist'>
-                        <FontAwesomeIcon icon={faPlus} className='icon' />
-                        <p>Danh sách</p>
-                    </button>
+                    
+                        {/* <FontAwesomeIcon icon={faHeart} className='text-[20px] text-red-500 group-hover:text-white' />
+                        <p>Yêu thích </p> */}
+                   
+                    <button className='playlist group'
+                    
+                                        onClick={() => {
+                                            if (allFavoriteFilms.some((favorfilm) => favorfilm._id == film._id)) {
+                                                handleDeleteFavoriteFilm(film._id)
+                                            }
+                                            else {
+                                                handleAddFavoriteFilm(film._id)
+                                            }
+                                        }}
+                                    >
+                                        <FontAwesomeIcon icon={faHeart}  className={`size-[20px] lg:size-5 group-hover:brightness-[1.5] group-hover:cursor-pointer group-hover:transition-all opacity-60 mr-3 ${allFavoriteFilms.some((favorfilm) => favorfilm._id == film._id) ? 'text-[red] brightness-[1.2]' : ''}`} />
+
+                                        <p >Yêu thích</p>
+                                    </button>
                 </div>
             </div>
             <div className='movie-detail w-full lg:w-1/2 pl-10 pr-5 pt-8 border invisible'>
@@ -74,7 +212,7 @@ const Film = ({ film }) => {
                     <li className='ml-3 mr-3 text-white'>|</li>
                     <li className='text-green-500 font-medium rating'>
                         {film.imdb.rating == null ? "10" : film.imdb.rating.toFixed(1)}
-                        <FontAwesomeIcon icon={faStar} className='ml-1 text-green-500' />
+                        <FontAwesomeIcon icon={faStar} color='red' className='ml-1 text-green-500' />
                     </li>
                     <div className='w-[150px] relative ml-3'>
                         <button
@@ -112,8 +250,8 @@ const Film = ({ film }) => {
                         <p>Phát</p>
                     </button>
                     <button className='playlist'>
-                        <FontAwesomeIcon icon={faPlus} className='icon' />
-                        <p>Danh sách</p>
+                        <FontAwesomeIcon icon={faHeart} className='icon text-red-500'  />
+                        <p>Yêu thích</p>
                     </button>
                 </div>
             </div>
